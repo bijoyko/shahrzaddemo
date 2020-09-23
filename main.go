8a190303c85a116c0ca3bf88746d712359dc6319
bijoyko <er.bijoyk@gmail.com>
2020-09-23 17:14:22 +0530

    flawless first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	router := gin.Default()
+	router.Use(gin.Logger())
+	// router.Static("/assets", "./assets")
+	router.LoadHTMLGlob("*.html")
+
+	router.GET("/", mainPage)
+	router.POST("/enterlink", openPage)
+	router.Run(":" + port)
+	// router.Run(":8080")
+}
+
+func mainPage(c *gin.Context) {
+	t, _ := template.ParseFiles("main.html")
+	t.Execute(c.Writer, nil)
+}
+
+func openPage(c *gin.Context) {
+
+	if c.PostForm("Link") != "" {
+		link1 := "https://xd.adobe.com/embed/"
+		link2 := c.PostForm("Link")
+		link := link1 + link2
+
+		t1, err := template.ParseFiles("index.html")
+		if err != nil {
+			log.Println(err)
+		}
+
+		t1.Execute(c.Writer, link)
+	} else {
+		text1 := "Please enter the appropriate url string!"
+		t2, err := template.ParseFiles("main.html")
+		if err != nil {
+			log.Println(err)
+		}
+		t2.Execute(c.Writer, text1)
+	}
+
+}
